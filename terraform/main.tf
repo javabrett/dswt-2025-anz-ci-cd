da6e6638d4fe03d29a553be179a37394072bd322
Brett Randall <brett.randall@confluent.io>
2025-04-27 17:46:03 +1000

    Working out CC provider config within module.

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,12 @@ provider "aws" {
   region = var.backend_region
 }
 
+provider "confluent" {
+  cloud_api_key    = var.confluent_cloud_api_key
+  cloud_api_secret = var.confluent_cloud_api_secret
+}
+
 module "confluent_cloud" {
   source      = "./modules/infrastructure/confluent-cloud"
   environment = var.environment
-  confluent_cloud_api_key = var.confluent_cloud_api_key
-  confluent_cloud_api_secret = var.confluent_cloud_api_secret
-}
 }
--- terraform/modules/infrastructure/confluent-cloud/main.tf
+++ terraform/modules/infrastructure/confluent-cloud/main.tf
@@ -7,11 +7,6 @@ terraform {
   }
 }
 
-provider "confluent" {
-  cloud_api_key    = var.confluent_cloud_api_key
-  cloud_api_secret = var.confluent_cloud_api_secret
-}
-
 resource "confluent_kafka_cluster" "this" {
   display_name = "${var.environment}-cluster"
 }
