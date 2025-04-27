ac1ac6cd5b4e89706f9fbcfe9a3ec4ff2d7b0d02
Brett Randall <brett.randall@confluent.io>
2025-04-27 17:23:48 +1000

    Also added CC API key/secret vars to main.

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,4 +5,7 @@ provider "aws" {
 module "confluent_cloud" {
   source      = "./modules/infrastructure/confluent-cloud"
   environment = var.environment
+  confluent_cloud_api_key = var.confluent_cloud_api_key
+  confluent_cloud_api_secret = var.confluent_cloud_api_secret
+}
 }
