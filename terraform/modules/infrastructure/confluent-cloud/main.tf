6789a2fc36850bb93536b124e63d8541fd124b66
Brett Randall <brett.randall@confluent.io>
2025-04-27 18:21:04 +1000

    Updated CC config.

--- terraform/modules/infrastructure/confluent-cloud/main.tf
+++ terraform/modules/infrastructure/confluent-cloud/main.tf
@@ -7,9 +7,17 @@ terraform {
   }
 }
 
+resource "confluent_environment" "this" {
+  display_name = "${var.environment}-environment"
+}
+
 resource "confluent_kafka_cluster" "this" {
   display_name = "${var.environment}-cluster"
   cloud = "AWS"
   region = "ap-southeast-2"
   basic {}
+  availability = "LOW"
+  environment {
+    id = confluent_environment.this.id
+  }
 }
