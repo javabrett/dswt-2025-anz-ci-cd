5eb42c7687044c2d5b5cc51af02279f60da5a792
Brett Randall <brett.randall@confluent.io>
2025-04-27 23:07:36 +1000

    Removed topic1.

--- terraform/modules/infrastructure/confluent-cloud/main.tf
+++ terraform/modules/infrastructure/confluent-cloud/main.tf
@@ -76,14 +76,3 @@ resource "confluent_kafka_topic" "topic0" {
   }
 }
 
-resource "confluent_kafka_topic" "topic1" {
-  kafka_cluster {
-    id = confluent_kafka_cluster.this.id
-  }
-  topic_name    = "topic1"
-  rest_endpoint = confluent_kafka_cluster.this.rest_endpoint
-  credentials {
-    key    = confluent_api_key.this.id
-    secret = confluent_api_key.this.secret
-  }
-}
